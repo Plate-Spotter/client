b879e25e1af49777166e12e36ef510f80fe0ebce
Jesse Thomas <jgthomas3@gmail.com>
2025-01-28 20:46:15 -0700

    Add: StartGame page

--- src/pages/StartGame.js
+++ src/pages/StartGame.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./StartGame.css";
+import StartGameCard from "../components/StartGameCard";
 
 function StartGame() {
 
   return (
-    <div className="display">
-      <h3>Start a Game, friend</h3>
-      <h3>Other Friends...coming soon</h3>
-      <button>Start Game</button>
-    </div>
+    <StartGameCard/>
   )
 
 };
