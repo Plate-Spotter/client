d2fe502fcc59a65bc7cd8a31b00c49bbbeb16cd2
Jesse Thomas <jgthomas3@gmail.com>
2025-02-15 11:20:52 -0700

    Add: Rerouting code for StartGameButton

--- src/components/StartGameButton.js
+++ src/components/StartGameButton.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { postNewGame } from "../api/UsersApi";
+import { useNavigate } from "react-router-dom";
 
 function StartGameButton({ gameName, gameDate, userId }) {
+  const navigate = useNavigate();
 
   const handleStartGame = async(e) => {
+
     e.preventDefault();
-    console.log("Game Name:", gameName);
-    console.log("Game Date:", gameDate);
-    console.log("Starting game...");
     try {
       const gameData = await postNewGame(gameName, gameDate, userId);
-      console.log("Game data:", gameData);
+
+      if (gameData?.id) {
+        console.log("Game ID StartGameBUtton: ", gameData.id);
+        navigate(`/game-session`, { state: { gameId: gameData.id } });
+      } else {
+        console.error("Game ID not found in response data");
+      }
     } catch (error) {
       console.log(error);
     }
